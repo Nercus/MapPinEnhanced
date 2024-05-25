96953813edbb54f4752c54911e60e77bc1effcd6
Nercus <s.wintersperger@me.com>
2024-05-25 23:25:17 +0200

    add some todos to global provider

--- core/providers/global.lua
+++ core/providers/global.lua
@@ -7,6 +7,9 @@ local PinProvider = Wayfinder:GetModule("PinProvider")
 ---@class PinManager
 local PinManager = Wayfinder:GetModule("PinManager")
 
+
+--- TODO: add a function to register a callback so other addons now when a pin is added
+--- TODO: add a wrapper to parse tomtom pin info
 local globalWayfinder = {}
 globalWayfinder.AddPin = PinManager.AddPin
 
