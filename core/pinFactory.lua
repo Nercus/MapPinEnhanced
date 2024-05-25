ab0f399f0b24c97042a9a3998929ec77ebaa57d9
Nercus <s.wintersperger@me.com>
2024-05-26 01:09:25 +0200

    annotation fixes

--- core/pinFactory.lua
+++ core/pinFactory.lua
@@ -6,16 +6,21 @@ local PinFactory = Wayfinder:CreateModule("PinFactory")
 
 local HBDP = LibStub("HereBeDragons-Pins-2.0")
 
-local WorldmapPool = CreateFramePool("Button", nil, "WayfinderWorldmapPinTemplate") ---@as Frame
-local MinimapPool = CreateFramePool("Frame", nil, "WayfinderMinimapPinTemplate") ---@as Frame
+local WorldmapPool = CreateFramePool("Button", nil, "WayfinderWorldmapPinTemplate")
+local MinimapPool = CreateFramePool("Frame", nil, "WayfinderMinimapPinTemplate")
 
 ---@param pinData pinData
 ---@param pinID string
 ---@return PinObject
 function PinFactory:CreatePin(pinData, pinID)
     local worldPin = WorldmapPool:Acquire()
+    ---@cast worldPin WayfinderWorldMapPinMixin
     local minimapPin = MinimapPool:Acquire()
+    ---@cast minimapPin WayfinderMinimapPinMixin
     local x, y, mapID = pinData.x, pinData.y, pinData.mapID
+
+    worldPin:Setup(pinData)
+    minimapPin:Setup(pinData)
 
     HBDP:AddWorldMapIconMap(Wayfinder, worldPin, mapID, x, y, 3, "PIN_FRAME_LEVEL_ENCOUNTER")
     HBDP:AddMinimapIconMap(Wayfinder, minimapPin, mapID, x, y, false, false)
