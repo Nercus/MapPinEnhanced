7a890746f298df03f1ec422c62b2d83be14ecc63
Nercus <s.wintersperger@me.com>
2024-07-22 16:33:25 +0200

    cleanup

--- MapPinEnhanced.lua
+++ MapPinEnhanced.lua
@@ -6,16 +6,13 @@ local MapPinEnhanced = select(2, ...)
 
 MapPinEnhanced.version = C_AddOns.GetAddOnMetadata("MapPinEnhanced", "Version")
 MapPinEnhanced.addonName = AddOnName
-MapPinEnhanced.isWrath = WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC
+
 MapPinEnhanced.isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
-MapPinEnhanced.isClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+MapPinEnhanced.isClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC -- Note: Classic is not supported yet
+
 MapPinEnhanced.isTomTomLoaded = C_AddOns.IsAddOnLoaded("TomTom")
 
-
-local HereBeDragons = LibStub("HereBeDragons-2.0")
-local HereBeDragonsPins = LibStub("HereBeDragons-Pins-2.0")
-
-MapPinEnhanced.HBD = HereBeDragons
-MapPinEnhanced.HBDP = HereBeDragonsPins
+MapPinEnhanced.HBD = LibStub("HereBeDragons-2.0")
+MapPinEnhanced.HBDP = LibStub("HereBeDragons-Pins-2.0")
 ---@type MPHCallbackHandlerRegistry
 MapPinEnhanced.CB = MapPinEnhanced.CB or LibStub("CallbackHandler-1.0"):New(MapPinEnhanced)
