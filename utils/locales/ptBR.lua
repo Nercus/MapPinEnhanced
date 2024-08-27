3f4a02964920dcc05aae10b33d75fda5604a424c
Nercus <48932939+Nercus@users.noreply.github.com>
2024-08-27 22:26:20 +0200

    implement loading localizations from curseforge through packager

--- utils/locales/ptBR.lua
+++ utils/locales/ptBR.lua
@@ -7,3 +7,5 @@ if LOCALE ~= "ptBR" then return end
 
 ---@class Locale
 local L = MapPinEnhanced.L
+--@localization(locale="ptBR", format="lua_additive_table")@
+-- If you want to help translate MapPinEnhanced into your language, please go to the following link: https://legacy.curseforge.com/wow/addons/mappinenhanced/localization
