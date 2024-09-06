7e1eec6406e1771e286330e4a939a70440e3c1fe
Nercus <48932939+Nercus@users.noreply.github.com>
2024-09-06 16:26:01 +0200

    move sections to pinmanager

--- modules/pins/pinManager/pinSection.lua
+++ modules/pins/pinManager/pinSection.lua
@@ -1,15 +1,14 @@
 ---@class MapPinEnhanced
 local MapPinEnhanced = select(2, ...)
----@class PinTracker
+---@class PinManager
 ---@field Sections PinSection[]
-local PinTracker = MapPinEnhanced:GetModule("PinTracker")
+local PinManager = MapPinEnhanced:GetModule("PinManager")
 local Events = MapPinEnhanced:GetModule("Events")
 local Textures = MapPinEnhanced:GetModule("Textures")
 local PinFactory = MapPinEnhanced:GetModule("PinFactory")
 local Utils = MapPinEnhanced:GetModule("Utils")
 
 local L = MapPinEnhanced.L
-
 
 ---@class SectionInfo
 ---@field name string
@@ -18,7 +17,7 @@ local L = MapPinEnhanced.L
 
 
 ---@param sectionInfo SectionInfo
-function PinTracker:RegisterSection(sectionInfo)
+function PinManager:RegisterSection(sectionInfo)
     assert(sectionInfo.name, "Section name is required")
     assert(sectionInfo.icon, "Section icon is required")
     assert(sectionInfo.source, "Section source is required")
@@ -36,6 +35,7 @@ function PinTracker:RegisterSection(sectionInfo)
 
     ---@param pinData pinData
     function section:AddPin(pinData)
+        -- TODO: find a way to structure pins and sections better -> there is only one tracked pin across all sections -> needs a single source of truth for all pins
         local sectionID = Utils:GenerateUUID(string.format("section-%s", sectionInfo.name))
         local pin = PinFactory:CreatePin(pinData, sectionID)
         table.insert(self.pins, pin)
@@ -78,7 +78,7 @@ function PinTracker:RegisterSection(sectionInfo)
     table.insert(self.Sections, section)
 end
 
-function PinTracker:GetSectionsBySource(source)
+function PinManager:GetSectionsBySource(source)
     local sections = {}
     for _, section in ipairs(self.Sections) do
         if section.info.source == source then
@@ -88,7 +88,7 @@ function PinTracker:GetSectionsBySource(source)
     return sections
 end
 
-function PinTracker:GetSectionByName(name)
+function PinManager:GetSectionByName(name)
     for _, section in ipairs(self.Sections) do
         if section.info.name == name then
             return section
@@ -96,7 +96,7 @@ function PinTracker:GetSectionByName(name)
     end
 end
 
-function PinTracker:UnregisterSectionByName(name)
+function PinManager:UnregisterSectionByName(name)
     for i, section in ipairs(self.Sections) do
         if section.info.name == name then
             table.remove(self.Sections, i)
@@ -105,7 +105,7 @@ function PinTracker:UnregisterSectionByName(name)
     end
 end
 
-function PinTracker:UnregisterSectionBySource(source)
+function PinManager:UnregisterSectionBySource(source)
     assert(source, "Source is required")
     assert(source ~= "MapPinEnhanced", "Cannot unregister default sections")
     for i, section in ipairs(self.Sections) do
@@ -115,7 +115,11 @@ function PinTracker:UnregisterSectionBySource(source)
     end
 end
 
-function PinTracker:LoadDefaultSections()
+function PinManager:GetAllSections()
+    return self.Sections
+end
+
+function PinManager:LoadDefaultSections()
     self:RegisterSection({
         name = L["Uncategorized Pins"],
         icon = Textures:GetTexture("PinTrackedYellow"),
@@ -130,5 +134,5 @@ function PinTracker:LoadDefaultSections()
 end
 
 Events:RegisterEvent("PLAYER_LOGIN", function()
-    PinTracker:LoadDefaultSections()
+    PinManager:LoadDefaultSections()
 end)
