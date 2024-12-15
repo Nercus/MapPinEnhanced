0c1fb0213e10d26461d3e2139e3e4d1f7f406298
Nercus <48932939+Nercus@users.noreply.github.com>
2024-12-15 20:30:46 +0100

    add tomtom shim

--- modules/providers/tomtom.lua
+++ modules/providers/tomtom.lua
@@ -9,10 +9,15 @@ local isHooked = false
 --- Hook TomTom's AddWaypoint function to add pins to the map when a use has TomTom installed and adds a waypoint to the map.
 local function HookTomTomAddWaypoint()
     if isHooked then return end
+    if not MapPinEnhanced.isTomTomLoaded then return end -- only hook if TomTom is loaded
     if not TomTom then return end
     if not TomTom.AddWaypoint then return end
     hooksecurefunc(TomTom, "AddWaypoint", function(_, ...)
         local mapID, x, y, info = ...
+        if not info then
+            info = {}
+            info.title = "TomTom Waypoint"
+        end
         if not mapID or not x or not y then return end
         PinManager:AddPin({
             mapID = mapID,
@@ -24,9 +29,32 @@ local function HookTomTomAddWaypoint()
     end)
 end
 
-MapPinEnhanced:RegisterEvent("ADDON_LOADED", function(_, addon)
-    if addon == "TomTom" then
-        MapPinEnhanced.isTomTomLoaded = true
+MapPinEnhanced:RegisterEvent("ADDON_LOADED", function(_, addonName)
+    if addonName == "TomTom" then
         HookTomTomAddWaypoint()
     end
 end)
+
+local function AddTomTomShim()
+    if TomTom then return end
+    if MapPinEnhanced.isTomTomLoaded then return end -- only shim if TomTom is not loaded
+    local TomTom = {}
+    TomTom.AddWaypoint = function(_, ...)
+        local mapID, x, y, info = ...
+        if not info then
+            info = {}
+            info.title = "TomTom Waypoint"
+        end
+        if not mapID or not x or not y then return end
+        PinManager:AddPin({
+            mapID = mapID,
+            x = x,
+            y = y,
+            title = info.title,
+            setTracked = true,
+        })
+    end
+    _G.TomTom = TomTom
+end
+
+MapPinEnhanced:RegisterEvent("PLAYER_LOGIN", AddTomTomShim)
