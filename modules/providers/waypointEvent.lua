25c5e0e65037b33d3f2bfb072d9e4ba7bf48a5e7
Nercus <48932939+Nercus@users.noreply.github.com>
2025-04-24 20:49:48 +0200

    fix: improve blocked addon handling in SetUserWaypoint function

--- modules/providers/waypointEvent.lua
+++ modules/providers/waypointEvent.lua
@@ -5,11 +5,29 @@ local MapPinEnhanced = select(2, ...)
 local PinProvider = MapPinEnhanced:GetModule("PinProvider")
 local PinManager = MapPinEnhanced:GetModule("PinManager")
 
+
+-- block SetUserWaypoint calls from other addons which should not trigger the pin provider
+-- MapPinEenhanced is in here as well to prevent infinite loops when the pin provider is used to set a waypoint
+local BLOCKED_ADDON_LIST = {
+    'WorldQuestTracker',
+    MapPinEnhanced.addonName,
+}
+
+
+local function isBlockedAddon(stack)
+    for _, blockedAddon in ipairs(BLOCKED_ADDON_LIST) do
+        if stack.find(stack, blockedAddon) then
+            return true
+        end
+    end
+    return false
+end
+
 ---@param uiMapPoint UiMapPoint
 local function HandleSetUserWaypoint(uiMapPoint)
     if not uiMapPoint then return end
     local stack = debugstack(2) ---@type string
-    if stack.find(stack, MapPinEnhanced.addonName) then return end -- ignore calls from this function
+    if isBlockedAddon(stack) then return end -- ignore calls from this function
     local title, texture, usesAtlas = PinProvider:DetectMouseFocusPinInfo()
     local isSuperTrackingCorpse = C_SuperTrack.IsSuperTrackingCorpse()
     PinManager:AddPin({
