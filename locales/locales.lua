38debc9d80d0e663457e8fa607e3f0e4c6f24018
Nercus <48932939+Nercus@users.noreply.github.com>
2025-06-15 22:59:30 +0200

    chore: add locales

---
+++ locales/locales.lua
@@ -0,0 +1,28 @@
+---@class MapPinEnhanced
+local MapPinEnhanced = select(2, ...)
+
+-- might want to check out using the @localization@ in the future for curseforge translations
+-- *** Using Phanx's method to localize strings ***
+-- You can find the original code here: https://phanx.net/addons/tutorials/localize
+local L = setmetatable({}, {
+    __index = function(t, k)
+        local v = tostring(k)
+        --@do-not-package@
+        if MapPinEnhanced.Debug then
+            MapPinEnhanced:Debug("Missing localization for: " .. v)
+        else
+            -- Delay the error message by 1 second if the locale is called before the full addon is loaded
+            C_Timer.After(1, function()
+                if MapPinEnhanced.Debug then
+                    MapPinEnhanced:Debug("Missing localization for: " .. v)
+                end
+            end)
+        end
+        --@end-do-not-package@
+        rawset(t, k, v)
+        return v
+    end
+})
+
+MapPinEnhanced.L = L
+MapPinEnhanced.locale = GetLocale()
