d0eac252e0a78e1accba750d4ddde1b675bec5e8
Nercus <48932939+Nercus@users.noreply.github.com>
2025-06-15 22:58:51 +0200

    remove libs and add lib install script

---
+++ install-deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# clear libs folder but keep the _imports.xml file
+find ./libs -mindepth 1 ! -name '_imports.xml' -exec rm -rf {} +
+
+# Libstub
+svn checkout https://repos.curseforge.com/wow/libstub/trunk ./libs/LibStub
+
+# CallbackHandler
+svn checkout https://repos.curseforge.com/wow/callbackhandler/trunk/CallbackHandler-1.0 ./libs/CallbackHandler-1.0
+
+# LibDBIcon
+svn checkout https://repos.curseforge.com/wow/libdbicon-1-0/trunk/LibDBIcon-1.0 ./libs/LibDBIcon-1.0
+
+# HereBeDragons
+git clone https://repos.wowace.com/wow/herebedragons ./libs/HereBeDragons
+
+# LibDataBroker
+git clone https://github.com/tekkub/libdatabroker-1-1 ./libs/LibDataBroker-1.1
+
+# NercUtils
+git clone https://github.com/Nercus/NercUtils ./libs/NercUtils
