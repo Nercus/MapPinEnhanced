0805ac616b860e26b30f62e8be8b38e75f4283df
Nercus <48932939+Nercus@users.noreply.github.com>
2025-06-17 13:22:01 +0200

    feat: add ticks to the slider

--- modules/templates/slider.lua
+++ modules/templates/slider.lua
@@ -4,6 +4,8 @@
 ---@field Forward Button
 MapPinEnhancedSliderMixin = {}
 
+---@class MapPinEnhancedSliderTickTemplate : Texture
+
 ---@param value number
 ---@param step number
 ---@return string
@@ -11,12 +13,33 @@ local function roundValueToPrecision(value, step)
     if not step or step == 1 then
         return tostring(math.floor(value))
     end
-    local precision = string.len(tostring(step):match('%.%d+')) - 1
+    local precision = math.max(0, -math.floor(math.log10(step)))
     return string.format("%." .. precision .. "f", value)
 end
 
 
+function MapPinEnhancedSliderMixin:UpdateTicks()
+    self.tickPool:ReleaseAll()
+
+    -- calculate the number of ticks based on the slider's range and step, never draw more than 10 ticks
+    local minValue, maxValue = self:GetMinMaxValues()
+    local step = self:GetValueStep()
+    local numTicks = math.min(10, math.ceil((maxValue - minValue) / step))
+    local tickSpacing = (self:GetWidth() - 20) / numTicks -- 20 is for the left and right buttons
+    for i = 0, numTicks do
+        local tickValue = minValue + i * step
+        if tickValue <= maxValue then
+            local tick = self.tickPool:Acquire()
+            tick:SetPoint("TOPLEFT", self, "BOTTOMLEFT", i * tickSpacing + 5, 20) -- 10 is for the left button
+            tick:SetWidth(10)
+            tick:SetHeight(10)
+            tick:Show()
+        end
+    end
+end
+
 function MapPinEnhancedSliderMixin:OnLoad()
+    self.tickPool = CreateTexturePool(self, "ARTWORK", 1, "MapPinEnhancedSliderTickTemplate")
     -- hook into setscript and every time a script is set hook it and also hook the textformatting
     hooksecurefunc(self, "SetScript", function(_, scriptName, func)
         if scriptName == "OnValueChanged" then
@@ -27,6 +50,10 @@ function MapPinEnhancedSliderMixin:OnLoad()
                 end
             end)
         end
+    end)
+
+    hooksecurefunc(self, "SetValueStep", function(_, step)
+        self:UpdateTicks()
     end)
 end
 
