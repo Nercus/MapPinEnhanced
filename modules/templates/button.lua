55ea29affe27467909cc80b6e4db7a0b4e6c1c75
Nercus <48932939+Nercus@users.noreply.github.com>
2025-06-17 11:39:33 +0200

    feat: add icon template and extend button to take icons

--- modules/templates/button.lua
+++ modules/templates/button.lua
@@ -1,2 +1,55 @@
 ---@class MapPinEnhancedButtonTemplate : Button
+---@field icon MapPinEnhancedIconMixin
+---@field text FontString
 MapPinEnhancedButtonMixin = {};
+
+
+
+function MapPinEnhancedButtonMixin:OnLoad()
+end
+
+function MapPinEnhancedButtonMixin:Update()
+    -- 3 different states for the button
+    -- 1. Label only
+    -- 2. Icon only
+    -- 3. Icon left then label
+
+    local hasIcon = self.icon and self.icon.icon
+    local hasLabel = self:GetText() and self:GetText() ~= ""
+    self.icon:ClearAllPoints()
+    self.text:ClearAllPoints()
+    if hasIcon and not hasLabel then
+        self.icon:Show()
+        local buttonHeight = self:GetHeight()
+        local padding = buttonHeight * 0.3
+        self.text:SetText("")
+        self.text:SetAllPoints()
+        self.icon:SetPoint("TOPLEFT", self.text, "TOPLEFT", padding, -padding)
+        self.icon:SetPoint("BOTTOMRIGHT", self.text, "BOTTOMRIGHT", -padding, padding)
+    elseif hasLabel and not hasIcon then
+        self.icon:Hide()
+        self.text:SetAllPoints(self)
+        self.text:Show()
+    elseif hasIcon and hasLabel then
+        -- calculate the width of the label set the size of the icon to match the height of the button -> sum the width values add some padding and then position correctly
+        local labelHeight = self.text:GetLineHeight()
+        self.icon:SetSize(labelHeight, labelHeight)
+        local padding = labelHeight * 0.25
+        local labelOffset = labelHeight / 2 + padding
+        self.text:SetPoint("CENTER", self, "CENTER", labelOffset, 0)
+        self.icon:SetPoint("RIGHT", self.text, "LEFT", -padding, 0)
+        self.icon:Show()
+    end
+end
+
+---@param icon MapPinEnhancedIcon
+function MapPinEnhancedButtonMixin:SetIcon(icon)
+    self.icon:SetIconTexture(icon)
+    self:Update()
+end
+
+---@param label string
+function MapPinEnhancedButtonMixin:SetLabel(label)
+    self:SetText(label)
+    self:Update()
+end
---
+++ modules/templates/icon.lua
@@ -0,0 +1,41 @@
+---@class MapPinEnhancedIconMixin : Texture
+---@field icon string
+MapPinEnhancedIconMixin = {}
+
+---@enum (key) MapPinEnhancedIcon
+local ICON_TEXTURES = {
+    cross = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconCross_Yellow.png",
+    duplicate = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconDuplicate_Yellow.png",
+    edit = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconEdit_Yellow.png",
+    editor = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconEditor_Yellow.png",
+    export = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconExport_Yellow.png",
+    import = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconImport_Yellow.png",
+    lock = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconLock_Yellow.png",
+    map = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconMap_Yellow.png",
+    plus = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconPlus_Yellow.png",
+    search = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconSearch_Yellow.png",
+    sets = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconSets_Yellow.png",
+    settings = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconSettings_Yellow.png",
+    tick = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconTick_Yellow.png",
+    trash = "Interface\\AddOns\\MapPinEnhanced\\assets\\icons\\IconTrash_Yellow.png",
+}
+
+---@param icon? MapPinEnhancedIcon
+function MapPinEnhancedIconMixin:SetIconTexture(icon)
+    if icon then
+        -- Allow setting the icon directly if provided
+        self.icon = icon
+    end
+    assert(self.icon, "MapPinEnhancedIconMixin: SetIconTexture called without icon set")
+    local texturePath = ICON_TEXTURES[self.icon]
+    if not texturePath then
+        error("MapPinEnhancedIconMixin: Invalid icon name: " .. tostring(self.icon))
+    end
+    self:SetTexture(texturePath)
+end
+
+function MapPinEnhancedIconMixin:OnLoad()
+    -- only run on load when the icon is set as a keyvalue
+    if not self.icon then return end
+    self:SetIconTexture()
+end
