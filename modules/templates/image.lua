0308293422834d12fea1ad06fa9840f6ddaab554
Nercus <48932939+Nercus@users.noreply.github.com>
2025-06-21 01:07:21 +0200

    feat: add image component

---
+++ modules/templates/image.lua
@@ -0,0 +1,24 @@
+---@class MapPinEnhancedImageTemplate : Frame
+---@field image Texture
+MapPinEnhancedImageMixin = {}
+
+
+
+function MapPinEnhancedImageMixin:SetImage(imagePath)
+    self.imagePath = imagePath
+    if not self.imagePath or self.imagePath == "" then
+        self.image:Hide()
+        return
+    end
+    -- when there is a slash or backslash in the path, it is a file path and we use SetTexture
+    -- otherwise, it is a texture name and we use SetAtlas
+    if self.imagePath:find("[/\\]") then
+        self.image:SetTexture(self.imagePath)
+    else
+        self.image:SetAtlas(self.imagePath)
+    end
+end
+
+function MapPinEnhancedImageMixin:OnLoad()
+    self:SetImage(self.imagePath)
+end
