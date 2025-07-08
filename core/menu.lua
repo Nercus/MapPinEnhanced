37792cc59753a385dce12c3e13cb2af5446db755
Nercus <48932939+Nercus@users.noreply.github.com>
2025-07-08 22:43:45 +0200

    chore: added another assert for submenus

--- core/menu.lua
+++ core/menu.lua
@@ -73,6 +73,7 @@ local function GenerateMenuElement(rootDescription, entry)
         templateEl:AddInitializer(entry.initializer)
         return templateEl
     elseif entry.type == "submenu" then
+        assert(entry.entry, "Entry for the submenu type of the submenu trigger")
         assert(entry.entry.type == "button" or entry.entry.type == "checkbox" or entry.entry.type == "radio" or
             entry.entry.type == "template", "Submenu entry must be a button, checkbox, radio or template")
         ---@diagnostic disable-next-line: missing-parameter for submenus the second and third parameter are not used
