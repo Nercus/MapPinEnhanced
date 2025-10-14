ead1f363cdab1063da012167b03205bd60ba90f0
Sebastian Wintersperger <s.wintersperger@me.com>
2025-10-14 22:33:16 +0200

    chore: test new background asset

--- modules/options/templates/OptionsFrame.lua
+++ modules/options/templates/OptionsFrame.lua
@@ -12,20 +12,21 @@ MapPinEnhancedOptionsFrameMixin = {}
 
 
 function MapPinEnhancedOptionsFrameMixin:OnLoad()
-    TabSystemOwnerMixin.OnLoad(self);
+    -- TabSystemOwnerMixin.OnLoad(self);
     self:SetPortraitTextureRaw("Interface\\AddOns\\MapPinEnhanced\\assets\\logo.png")
-    self:SetTabSystem(self.TabSystem);
+    self.Bg:SetTexture("Interface\\AddOns\\MapPinEnhanced\\assets\\bg.png");
+    -- self:SetTabSystem(self.TabSystem);
 
-    ---@type number
-    local optionsTabID = self:AddNamedTab("options", self.optionsView);
-    ---@type number
-    local importTabID = self:AddNamedTab("import", self.importView);
-    ---@type number
-    local setEditorTabID = self:AddNamedTab("seteditor", self.seteditorView)
-    self.views = {
-        [optionsTabID] = self.optionsView,
-        [importTabID] = self.importView,
-        [setEditorTabID] = self.seteditorView,
-    }
-    TabSystemOwnerMixin.SetTab(self, optionsTabID);
+    -- ---@type number
+    -- local optionsTabID = self:AddNamedTab("options", self.optionsView);
+    -- ---@type number
+    -- local importTabID = self:AddNamedTab("import", self.importView);
+    -- ---@type number
+    -- local setEditorTabID = self:AddNamedTab("seteditor", self.seteditorView)
+    -- self.views = {
+    --     [optionsTabID] = self.optionsView,
+    --     [importTabID] = self.importView,
+    --     [setEditorTabID] = self.seteditorView,
+    -- }
+    -- TabSystemOwnerMixin.SetTab(self, optionsTabID);
 end
