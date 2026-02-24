cf5952fe15a3362b959c78fac7fafcfabd575fb7
Sebastian Wintersperger <s.wintersperger@me.com>
2026-02-24 22:35:45 +0100

    chore: remove auto load of options for testing

--- modules/options/optionsFrame.lua
+++ modules/options/optionsFrame.lua
@@ -75,7 +75,6 @@ function MapPinEnhancedOptionsFrameMixin:OnLoad()
     MapPinEnhanced:AddSlashCommand("options", function()
         self:ToggleOptionsFrame()
     end, "Open the options frame")
-    self:ToggleOptionsFrame()
 end
 
 function MapPinEnhancedOptionsFrameMixin:OnShow()
