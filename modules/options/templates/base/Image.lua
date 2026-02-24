5fa397d7182a4fc25a10abdff97927b354f60442
Sebastian Wintersperger <s.wintersperger@me.com>
2026-02-24 11:46:34 +0100

    chore: add resize logic for images

--- modules/options/templates/base/Image.lua
+++ modules/options/templates/base/Image.lua
@@ -1,27 +1,11 @@
 ---@class MapPinEnhancedImageTemplate : Frame
 ---@field image Texture
 ---@field imagePath string
----@field sourceWidth number
----@field sourceHeight number
 MapPinEnhancedImageMixin = {}
 
-
-function MapPinEnhancedImageMixin:UpdateHeight()
-    assert(self.sourceWidth and self.sourceHeight, "sourceWidth and sourceHeight must be set")
-    local aspectRatio = self.sourceHeight / self.sourceWidth
-    local currentWidth = self:GetWidth()
-    local newHeight = Round(currentWidth * aspectRatio)
-    self:SetHeight(newHeight)
-    self.lastWidth = currentWidth
-end
-
 ---@param imagePath string
----@param sourceWidth number
----@param sourceHeight number
-function MapPinEnhancedImageMixin:SetImage(imagePath, sourceWidth, sourceHeight)
+function MapPinEnhancedImageMixin:SetImage(imagePath)
     self.imagePath = imagePath
-    self.sourceWidth = sourceWidth or self.sourceWidth
-    self.sourceHeight = sourceHeight or self.sourceHeight
     if not self.imagePath or self.imagePath == "" then
         self.image:Hide()
         return
@@ -34,16 +18,8 @@ function MapPinEnhancedImageMixin:SetImage(imagePath, sourceWidth, sourceHeight)
         self.image:SetAtlas(self.imagePath)
     end
     self.image:Show()
-    self:UpdateHeight()
-end
-
-function MapPinEnhancedImageMixin:OnSizeChanged()
-    local currentWidth = self:GetWidth()
-    if not self.lastWidth or self.lastWidth ~= currentWidth then
-        self:UpdateHeight()
-    end
 end
 
 function MapPinEnhancedImageMixin:OnLoad()
-    self:SetImage(self.imagePath, self.sourceWidth, self.sourceHeight)
+    self:SetImage(self.imagePath)
 end
