956483c7e42c9f9b596cfd386a3d058c51d3c61f
Sebastian Wintersperger <s.wintersperger@me.com>
2026-03-12 00:15:42 +0100

    chore: add icon to group entries on tracker

--- modules/tracker/templates/GroupEntry.lua
+++ modules/tracker/templates/GroupEntry.lua
@@ -6,6 +6,7 @@ local MapPinEnhanced = select(2, ...)
 ---@field group MapPinEnhancedGroupMixin
 ---@field expandButton MapPinEnhancedTrackerGroupEntryExpandButton
 ---@field title FontString
+---@field icon Texture
 MapPinEnhancedTrackerGroupEntryMixin = {}
 
 ---@class MapPinEnhancedTrackerGroupEntryExpandButton : Button
@@ -22,16 +23,22 @@ function MapPinEnhancedTrackerGroupEntryMixin:UpdateCollapseButton()
     end
 end
 
+function MapPinEnhancedTrackerGroupEntryMixin:SetIcon(texturePath)
+    self.icon:SetTexture(texturePath)
+end
+
 function MapPinEnhancedTrackerGroupEntryMixin:Reset()
     self:UpdateCollapseButton()
 end
 
 ---@param treeNode TreeNodeMixin
 function MapPinEnhancedTrackerGroupEntryMixin:Init(treeNode)
+    ---@class MapPinEnhancedGroupMixin
     local group = treeNode:GetData()
     self.group = group
     self.treeNode = treeNode
     self:SetTitle(group:GetName())
+    self:SetIcon(group:GetIcon())
     self:UpdateCollapseButton()
 end
 
