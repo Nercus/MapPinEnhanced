81c2c61b246261e34a9c442a6c07dc6606f35e6f
Sebastian Wintersperger <s.wintersperger@me.com>
2026-03-14 22:19:16 +0100

    Revert "chore: move globals to its own folder"
    This reverts commit 6eae8b9cd9931078f2433090bb02fe4516c1e656.

--- MapPinEnhanced.lua
+++ MapPinEnhanced.lua
@@ -19,3 +19,20 @@ MapPinEnhanced.Chomp = LibStub:GetLibrary("Chomp")
 
 MapPinEnhanced.basePath = "Interface\\AddOns\\MapPinEnhanced"
 MapPinEnhanced.assetsPath = MapPinEnhanced.basePath .. "\\assets"
+
+---@type table<string, function>
+local globalAPI = {}
+---@type table
+_G[MapPinEnhanced.name] = globalAPI
+function MapPinEnhanced:RegisterGlobalAPI(name, func)
+    assert(name, "MapPinEnhanced:RegisterGlobalAPI: name is nil")
+    assert(type(name) == "string", "MapPinEnhanced:RegisterGlobalAPI: name must be a string")
+    assert(func, "MapPinEnhanced:RegisterGlobalAPI: func is nil")
+    assert(type(func) == "function", "MapPinEnhanced:RegisterGlobalAPI: func must be a function")
+
+    if not globalAPI[name] then
+        globalAPI[name] = func
+    else
+        error("MapPinEnhanced:RegisterGlobalAPI: API with name '" .. name .. "' already exists")
+    end
+end
