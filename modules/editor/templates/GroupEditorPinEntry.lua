ec5b14326a0a448508e34323cfd9a9e7b8a1f31e
Nercus <48932939+Nercus@users.noreply.github.com>
2026-08-02 22:58:37 +0100

    chore: adjust size of dropline for editor

--- modules/editor/templates/GroupEditorPinEntry.lua
+++ modules/editor/templates/GroupEditorPinEntry.lua
@@ -330,11 +330,11 @@ end
 function MapPinEnhancedEditorGroupEditorPinEntryMixin:SetDropTarget(placement)
     self.dropLine:ClearAllPoints()
     if placement == "before" then
-        self.dropLine:SetPoint("TOPLEFT", 48, 1)
-        self.dropLine:SetPoint("TOPRIGHT", -12, 1)
+        self.dropLine:SetPoint("TOPLEFT", 48, 2)
+        self.dropLine:SetPoint("TOPRIGHT", -12, 2)
     else
-        self.dropLine:SetPoint("BOTTOMLEFT", 48, -1)
-        self.dropLine:SetPoint("BOTTOMRIGHT", -12, -1)
+        self.dropLine:SetPoint("BOTTOMLEFT", 48, -2)
+        self.dropLine:SetPoint("BOTTOMRIGHT", -12, -2)
     end
     self.dropLine:Show()
 end
