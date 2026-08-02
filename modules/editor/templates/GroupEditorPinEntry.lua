8042ea13a77aca6e335454ac00ea09b0d01192c6
Nercus <48932939+Nercus@users.noreply.github.com>
2026-08-02 22:52:14 +0100

    chore: adjust dropline again for editor

--- modules/editor/templates/GroupEditorPinEntry.lua
+++ modules/editor/templates/GroupEditorPinEntry.lua
@@ -330,11 +330,11 @@ end
 function MapPinEnhancedEditorGroupEditorPinEntryMixin:SetDropTarget(placement)
     self.dropLine:ClearAllPoints()
     if placement == "before" then
-        self.dropLine:SetPoint("TOPLEFT", 48, -4)
-        self.dropLine:SetPoint("TOPRIGHT", -12, -4)
+        self.dropLine:SetPoint("TOPLEFT", 48, 0)
+        self.dropLine:SetPoint("TOPRIGHT", -12, 0)
     else
-        self.dropLine:SetPoint("BOTTOMLEFT", 48, 4)
-        self.dropLine:SetPoint("BOTTOMRIGHT", -12, 4)
+        self.dropLine:SetPoint("BOTTOMLEFT", 48, 0)
+        self.dropLine:SetPoint("BOTTOMRIGHT", -12, 0)
     end
     self.dropLine:Show()
 end
